f579ec3fa4df44a98ba1d5ab7b1ec6c79ef753b0
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2024-06-02 10:08:55 +1000

    feat(theme): Implement logic to change site theme
    - detect user preference for dark mode and apply themes
    - add functions to toggle dark mode and syntax highlight

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,59 @@
 function toggleToc() {
-  const tocElement = document.querySelector('.post-toc');
-  tocElement.classList.toggle('show');
+  const tocElement = document.querySelector(".post-toc");
+  tocElement.classList.toggle("show");
 
-  const tocToggler = document.querySelector('.toc-toggler');
-  tocToggler.classList.toggle('active');
+  const tocToggler = document.querySelector(".toc-toggler");
+  tocToggler.classList.toggle("active");
 }
+
+function setTheme(theme) {
+  const themeToggler = document.querySelector("#theme-toggle");
+  const themeCssElement = document.querySelector("link#syntax-theme");
+  const { sunIcon, moonIcon } = themeToggler.dataset;
+
+  // switch out syntax highlight theme
+  if (themeCssElement)
+    themeCssElement.href = `/syntax-theme-${theme}.css`;
+
+  if (theme == "dark") {
+    document.body.classList.add("dark");
+    sessionStorage.setItem("theme", "dark");
+    themeToggler.innerHTML = sunIcon;
+  } else {
+    document.body.classList.remove("dark");
+    sessionStorage.setItem("theme", "light");
+    themeToggler.innerHTML = moonIcon;
+  }
+}
+
+function toggleTheme() {
+  const currentTheme = sessionStorage.getItem("theme");
+
+  if (currentTheme == "light") {
+    setTheme("dark");
+  } else {
+    setTheme("light");
+  }
+}
+
+function initThemeToggle() {
+  const themeToggler = document.querySelector("#theme-toggle");
+  const preferDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+  const currentTheme = sessionStorage.getItem("theme");
+  const { sunIcon, moonIcon } = themeToggler.dataset;
+
+  themeToggler.addEventListener("click", () => toggleTheme());
+
+  if (currentTheme) {
+    setTheme(currentTheme);
+    return;
+  }
+
+  if (preferDarkMode.matches) {
+    setTheme("dark");
+  } else {
+    setTheme("light");
+  }
+}
+
+initThemeToggle();
